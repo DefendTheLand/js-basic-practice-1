3e1188766232145ff8a17d4e15a403cb20cb1d6c
James <patchouly98@gmail.com>
2017-06-03 22:31:55 +0800

    finished

--- main/main.js
+++ main/main.js
@@ -1,9 +1,31 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(x1,x2) {
+	if(x2===0||isNaN(x2)) {
+		return 'cant calculate'
+	} else {
+		return x1%x2;
+	} 
+};
 
-function caclSum() {};
+function caclSum(numArray) {
+	var sum = 0;
+	var length = numArray.length;
+	for(var i = 0;i < length;i++){
+		sum += numArray[i];
+	}
+	return sum;
+};
 
-function caclSumInConditon() {};
+function caclSumInConditon(numArray,x) {
+	var sum = 0;
+	var length = numArray.length;
+	for(var i = 0;i < length;i++){
+		if (numArray[i]<x) {
+			sum += numArray[i];
+		}
+	}
+	return sum;
+};
 
 module.exports = {
     caclRemaindar,
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const main = require('../main/main');
 
 describe('main()', () => {
